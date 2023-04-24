a7cc17e74f03235f4a9449571229a3e0d73f705d
Max Bayer <m_bayer@mail.ru>
2023-04-25 01:59:34 +0200

    Added view inside the main class

--- src/main/java/test/main.java
+++ src/main/java/test/main.java
@@ -1,15 +1,19 @@
 package test;
 import gdscript_language.*;
 import gdscript_language.listener.*;
+import org.antlr.v4.gui.TreeViewer;
 import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTree;
 import org.antlr.v4.runtime.tree.ParseTreeWalker;
 
+import javax.swing.*;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -33,10 +37,14 @@ public class main {
             GDScriptLexer y = new GDScriptLexer(x);
             ParseTreeWalker walker = new ParseTreeWalker();
             CommonTokenStream tokens = new CommonTokenStream(y);
-            GDScriptParser parser = new GDScriptParser(tokens);
-            NormalStmtListener listener = new NormalStmtListener();
 
+            GDScriptParser parser = new GDScriptParser(tokens);
+            ParseTree tree = parser.program();
+            NormalStmtListener listener = new NormalStmtListener();
             walker.walk(listener, parser.program());
+            viewAntlr4Tree(parser, tree);
+            System.out.println(tree.toStringTree(parser));
+
             Set<Integer> lineSet = new HashSet<>();
             Set<Integer> markedLines = new HashSet<>();
             for(GDScriptParser.StmtContext context: listener.getStmts()){
@@ -52,9 +60,23 @@ public class main {
         {
 
         }
-
-
-
-
     }
+    private static void viewAntlr4Tree(GDScriptParser parser, ParseTree tree)
+    {
+        JFrame frame = new JFrame("Antlr AST");
+        JPanel panel = new JPanel();
+        TreeViewer viewer = new TreeViewer(
+                Arrays.asList(
+                    parser.getRuleNames()
+                )
+                ,tree
+        );
+        viewer.setScale(1.2); // Scale a little
+        panel.add(viewer);
+        frame.add(panel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+
 }
