3089ce66bd678a3b58286e2e5253582dac492457
Max Bayer <m_bayer@mail.ru>
2023-04-28 21:03:33 +0200

    Created ANTLRViewer Class. Added Textfield which may be compiled into a ast

---
+++ src/main/java/ANTLR4_Viewer/ANTLR4Viewer.java
@@ -0,0 +1,126 @@
+package ANTLR4_Viewer;
+
+import gdscript_language.GDScriptLexer;
+import gdscript_language.GDScriptParser;
+import org.antlr.v4.gui.TreeViewer;
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTree;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class ANTLR4Viewer {
+
+    private static JFrame frame;
+    private static JPanel mainPanel;
+    private static JPanel antlrPanel;
+    private static String toParse;
+
+    public static void openViewer(){
+        createWindow();
+        toParse = createFileString("/home/gott/Dokumente/test/test.txt");
+        GDScriptParser parser = createParser(toParse);
+        viewAntlr4Tree(parser);
+        createTextField();
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    private static void createWindow(){
+        frame = new JFrame("ANTLR4Viewer");
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setSize(new Dimension(400, 200));
+        frame.setLayout(new GridBagLayout());
+        mainPanel = new JPanel();
+        mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.X_AXIS));
+        frame.add(mainPanel);
+        antlrPanel = new JPanel();
+    }
+    private static void createTextField()
+    {
+        JButton a = new JButton("parse");
+
+
+    JSplitPane splitPane = new JSplitPane();
+        splitPane.setOrientation(JSplitPane.VERTICAL_SPLIT);
+        splitPane.setResizeWeight(1);
+        splitPane.setBottomComponent(a);
+        JTextArea textArea = new JTextArea("");
+        textArea.setRows(10);
+        textArea.setColumns(10);
+        splitPane.setTopComponent(textArea);
+        mainPanel.add(splitPane);
+
+        a.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                toParse = textArea.getText();
+                updateANTLR4Tree();
+            }
+        });
+
+    }
+    private static void viewAntlr4Tree(GDScriptParser parser) {
+
+        parser.reset();
+        ParseTree tree = parser.program();
+
+        TreeViewer viewer = new TreeViewer(
+                Arrays.asList(
+                        parser.getRuleNames()
+                )
+                ,tree
+        );
+        viewer.setScale(1.2);
+
+        if(antlrPanel.getComponentCount() == 1)
+            antlrPanel.remove(0);
+        antlrPanel.add(viewer);
+        mainPanel.add(antlrPanel);
+    }
+
+    private static void updateANTLR4Tree(){
+        GDScriptParser parser = createParser(toParse);
+        viewAntlr4Tree(parser);
+        antlrPanel.updateUI();
+    }
+
+    public static GDScriptParser createParser(String stringToParse){
+        GDScriptParser parser;
+
+        CharStream x = CharStreams.fromString(stringToParse);
+        GDScriptLexer y = new GDScriptLexer(x);
+
+        CommonTokenStream tokens = new CommonTokenStream(y);
+        parser = new GDScriptParser(tokens);
+
+        return parser;
+    }
+
+    public static String createFileString(String path){
+        StringBuilder sb = new StringBuilder();
+        try {
+            File file = new File(path);
+            if (file.exists()) {
+                FileInputStream fis = new FileInputStream(file);
+                try (BufferedReader br = new BufferedReader(new InputStreamReader(fis))) {
+                    String line;
+                    while ((line = br.readLine()) != null) {
+                        sb.append(line);
+                        sb.append('\n');
+                    }
+                }
+            }
+        } catch(Exception ex) {
+        }
+        return sb.toString();
+    }
+}
--- src/main/java/test/main.java
+++ src/main/java/test/main.java
@@ -1,28 +1,17 @@
 package test;
+import ANTLR4_Viewer.ANTLR4Viewer;
 import gdscript_language.*;
 import gdscript_language.listener.*;
-import org.antlr.v4.gui.TreeViewer;
-import org.antlr.v4.runtime.CharStream;
-import org.antlr.v4.runtime.CharStreams;
-import org.antlr.v4.runtime.CommonTokenStream;
-import org.antlr.v4.runtime.tree.ParseTree;
 import org.antlr.v4.runtime.tree.ParseTreeWalker;
 
-
-import javax.swing.*;
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.InputStreamReader;
-import java.util.Arrays;
 
 
 public class main {
     public static void main(String[] args){
 
-        GDScriptParser parser = createParser();
-        testCode(parser);
-        viewAntlr4Tree(parser);
+        ANTLR4Viewer.openViewer();
+        //GDScriptParser parser = ANTLR4Viewer.createParser();
+        //testCode(parser);
     }
 
     private static void testCode(GDScriptParser parser){
@@ -51,54 +40,6 @@ public class main {
                 }
             }
         }
-    }
-
-    private static GDScriptParser createParser(){
-         GDScriptParser parser;
-        try {
-            File file = new File("/home/gott/Dokumente/test/test.txt");
-            StringBuilder sb = new StringBuilder();
-            if (file.exists()) {
-                FileInputStream fis = new FileInputStream(file);
-                try (BufferedReader br = new BufferedReader(new InputStreamReader(fis))) {
-                    String line;
-                    while ((line = br.readLine()) != null) {
-                        sb.append(line);
-                        sb.append('\n');
-                    }
-                }
-            }
-
-            CharStream x = CharStreams.fromString(sb.toString());
-            GDScriptLexer y = new GDScriptLexer(x);
-
-            CommonTokenStream tokens = new CommonTokenStream(y);
-            parser = new GDScriptParser(tokens);
-
-        }
-        catch(Exception ex) {
-            parser = null;
-        }
-        return parser;
-    }
-
-    private static void viewAntlr4Tree(GDScriptParser parser) {
-        parser.reset();
-        ParseTree tree = parser.program();
-        JFrame frame = new JFrame("Antlr AST");
-        JPanel panel = new JPanel();
-        TreeViewer viewer = new TreeViewer(
-                Arrays.asList(
-                    parser.getRuleNames()
-                )
-                ,tree
-        );
-        viewer.setScale(0.8); // Scale a little
-        panel.add(viewer);
-        frame.add(panel);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.pack();
-        frame.setVisible(true);
     }
 
 }
